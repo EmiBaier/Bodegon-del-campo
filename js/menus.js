9df86e28523a86619edf9dfc0355471bfc1d693d
baieremi <baieremiliano@gmail.com>
2019-08-22 01:39:27 -0300

    Sección menús

---
+++ js/menus.js
@@ -0,0 +1,52 @@
+function mostrarPestanas(n) {
+  var cabeceras = document.getElementsByClassName('cabeceras');
+  var pestanas = document.getElementsByClassName('pestanas');
+
+  for (var i = 0; i < pestanas.length; i++) {
+    if (pestanas[i].className.includes('p-activa')) {
+      pestanas[i].className = pestanas[i].className.replace('p-activa', '');
+      cabeceras[i].className = cabeceras[i].className.replace('c-activa', '');
+      break;
+    }
+  }
+  pestanas[n].className += ' p-activa';
+  cabeceras[n].className += ' c-activa';
+}
+
+// var menus = {
+//   "lunes": [
+//         "Plato1",
+//         "Plato2",
+//         "Plato3",
+//         "Plato4",
+//         "Plato5"
+//       ],
+//   "martes": [
+//         "Plato1",
+//         "Plato2",
+//         "Plato3",
+//         "Plato4",
+//         "Plato5"
+//       ],
+//   "miercoles": [
+//         "Plato1",
+//         "Plato2",
+//         "Plato3",
+//         "tu vieja en tangoide",
+//         "Plato5"
+//       ],
+//   "jueves": [
+//         "Plato1",
+//         "Plato2",
+//         "Plato3",
+//         "Plato4",
+//         "Plato5"
+//       ],
+//   "viernes": [
+//         "Plato1",
+//         "Plato2",
+//         "Plato3",
+//         "Plato4",
+//         "Plato5"
+//       ]
+// };
